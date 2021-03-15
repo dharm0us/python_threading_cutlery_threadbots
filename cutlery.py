236acf49b857ec1614887c5f7648ff5374810de0
Dharmendra Tolani <dharmendra.tolani@gmail.com>
2021-03-15 17:47:18 +0000

    use lock to avoid race conditions

--- cutlery.py
+++ cutlery.py
@@ -1,15 +1,18 @@
 from attr import attrs, attrib
+import threading
 
 @attrs
 class Cutlery:
     knives = attrib(default=0)
     forks = attrib(default=0)
+    lock = attrib(threading.Lock())
 
     def give(self, to: 'Cutlery', knives=0, forks=0):
         self.change(-knives, -forks)
         to.change(knives, forks)
 
     def change(self, knives, forks):
+         with self.lock:
             self.knives += knives
             self.forks += forks
 
