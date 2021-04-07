0a68ad7fd8758b80672d0b6e5e4faca2d49d3da8
Dharmendra Tolani <dharmendra.tolani@gmail.com>
2021-04-07 09:54:04 +0000

    now the async poller version won't depend on the startup order of server and client.

--- chapter4_zmq/poller_aio.py
+++ chapter4_zmq/poller_aio.py
@@ -5,23 +5,47 @@ from zmq.asyncio import Context
 
 context = Context()
 
-async def do_receiver():
+async def do_receiver_works_only_when_client_starts_after_server():
     receiver = context.socket(zmq.PULL)
     receiver.connect("tcp://localhost:5557")
     while message := await receiver.recv_json():
         print(f'Via PULL: {message}')
 
-async def do_subscriber():
+async def do_receiver_works_whether_client_or_server_starts_first():
+    receiver = context.socket(zmq.PULL)
+    receiver.connect("tcp://localhost:5557")
+    while True:
+        message = await receiver.recv_json()
+        if message:
+            print(f'Via PULL: {message}')
+        else:
+            continue
+
+
+async def do_subscriber_works_only_when_client_starts_after_server():
     subscriber = context.socket(zmq.SUB)
     subscriber.connect("tcp://localhost:5556")
     subscriber.setsockopt_string(zmq.SUBSCRIBE, '')
     while message := await subscriber.recv_json():
         print(f'Via SUB: {message}')
 
+async def do_subscriber_works_whether_client_or_server_starts_first():
+    subscriber = context.socket(zmq.SUB)
+    subscriber.connect("tcp://localhost:5556")
+    subscriber.setsockopt_string(zmq.SUBSCRIBE, '')
+    while True:
+        message = await subscriber.recv_json()
+        if message:
+            print(f'Via SUB: {message}')
+        else:
+            continue
+
 async def main():
     await asyncio.gather(
-        do_receiver(),
-        do_subscriber(),
+        #do_receiver_works_whether_client_or_server_starts_first(),
+        #do_subscriber_works_whether_client_or_server_starts_first(),
+        do_receiver_works_only_when_client_starts_after_server(),
+        do_subscriber_works_only_when_client_starts_after_server(),
     )
 
 asyncio.run(main())
--- chapter4_zmq/readme.md
+++ chapter4_zmq/readme.md
@@ -2,9 +2,31 @@ $ docker run -it python:3.8 bash
 
 Now you will see the bash prompt of this new docker container running python3.8.
 In this:
+
 $ pip install zmq
+
+With normal Zmq(i.e. without asyncio), you can start the client before the server or vice versa. Since the polling goes on in the infinite loop.
+So
+Either
 $ python poller.py &
-$ python poller_srv.py
+$ python poller_srv.py &
+
+OR
+$ python poller_srv.py &
+$ python poller.py &
+
+will work.
+
+But with async implementation, in the original code you have to start the server before the client else the client would get stuck and won't be able to come out of the loop.
+
+So only
+$ python poller_srv.py &
+$ python poller_aio.py &
+
+would work.
+
+But I have made some changes in poller_aio.py and you can see both the code versions - one of them will work irrespective of the starting order of client/server. The other is still dependent.
+
 Via PULL: 0
 Via SUB: 0
 Via PULL: 1
@@ -17,4 +39,3 @@ Via PULL: 4
 Via SUB: 4
 
 -----------------
-Ideally poller_aio.py should work the same way but I couldn't make it work.
